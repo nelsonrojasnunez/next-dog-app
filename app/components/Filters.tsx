1115d9827e01c540afd09b868192a2a054baf9d3
Nelson Rojas <nrojas@mobdev.cl>
2024-08-07 13:48:37 -0400

    Remove actions.ts to embed API calls inside the Filters component

--- app/components/Filters.tsx
+++ app/components/Filters.tsx
@@ -1,13 +1,30 @@
 "use client";
 
 import React, { useRef, useState } from "react";
-import { fetchDogs } from "../actions";
 import Selector from "./Selector";
 interface Props {
   breedList: string[];
   handleLoadDogsImages: (data: string[]) => void;
   handleSetLoading: (status: boolean) => void;
 }
+
+export async function fetchDogs(breeds: string, numberOfDogs: number) {
+  try {
+    const response = await fetch(
+      `https://dog.ceo/api/breed/${breeds}images/random/${numberOfDogs}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response for fetching dogs was not ok");
+    }
+    const data = await response.json();
+    console.log("** DOGS **", data.message);
+    return data.message;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
 const Filters = ({
   breedList,
   handleLoadDogsImages,
